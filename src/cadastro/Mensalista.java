82a45dbcbeefe1a6220671b036c9599474bc9012
gui s <guigudf@hotmail.com>
2021-04-26 21:24:03 -0300

    adicinado a classe mensalista e carro

---
+++ src/cadastro/Mensalista.java
@@ -0,0 +1,30 @@
+package cadastro;
+
+import java.util.ArrayList;
+
+public class Mensalista {
+    private String nome;
+    private String endereco;
+    private String celular;
+    private String telefone;
+    private int cnh;
+    private ArrayList<Carro> carros;
+    public String getNome() {
+        return nome;
+    }
+    public String getEndereco() {
+        return endereco;
+    }
+    public String getCelular() {
+        return celular;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+    public int getCnh() {
+        return cnh;
+    }
+    public ArrayList<Carro> getCarros() {
+        return carros;
+    }
+}
