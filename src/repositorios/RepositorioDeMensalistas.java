dcdadc1406313e6c505101736026c15a4272d2b0
Pablo Santos Costa <pablo.costa@tst.jus.br>
2021-05-02 17:06:14 -0300

    repositorios mensalista update

--- src/repositorios/RepositorioDeMensalistas.java
+++ src/repositorios/RepositorioDeMensalistas.java
@@ -5,10 +5,25 @@
  */
 package repositorios;
 
+import java.util.ArrayList;
+import cadastro.Mensalista;
 /**
  *
  * @author pablo
  */
 public class RepositorioDeMensalistas {
+    
+    private ArrayList<Mensalista> mensalistas = new ArrayList<Mensalista>;
+    public Mensalista buscarUmComCnh (int cnh){
+        for(Mensalista m: mensalistas){
+            if (m.getCnh() == cnh)
+            return m;
+        }
+        return null;
+    }
+    public Mensalista adicionarUm(Mensalista m) {
+        mensalistas.add(m);
+        return null;
+    }
     
 }
